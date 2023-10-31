471f3993893dba5c70bb199c86b35e137499ce30
Slobodan Kuzmanovic <slobodan.kuzmanovic@qualifacts.com>
2023-11-01 00:42:50 +0100

    Create API Controller

---
+++ ActiveVisitors.API/Controllers/ActiveVisitors.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ActiveVisitors.API.Controllers
+{
+    [ApiController]
+    [Route("api/active-visitors")]
+    public class ActiveVisitorsController : ControllerBase
+    {
+        [HttpGet(Name = "Index")]
+        public string Index(DateTime date, string camerraIds)
+        {
+            return "Slobodan";
+        }
+    }
+}
